a37c29683aedec751b16827c91558dda78f87cd5
Lmabert <liyunxin@52tt.com>
2021-11-18 18:13:16 +0800

    Delete FlutterBoost

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_boost/flutter_boost.dart';
-
-class CustomFlutterBinding extends WidgetsFlutterBinding
-    with BoostFlutterBinding {}
-
-///全局生命周期监听示例
-class AppLifecycleObserver with GlobalPageVisibilityObserver {
-  @override
-  void onBackground(Route route) {
-    super.onBackground(route);
-    print("AppLifecycleObserver - onBackground");
-  }
-
-  @override
-  void onForeground(Route route) {
-    super.onForeground(route);
-    print("AppLifecycleObserver - onForground");
-  }
-
-  @override
-  void onPagePush(Route route) {
-    super.onPagePush(route);
-    print("AppLifecycleObserver - onPagePush");
-  }
-
-  @override
-  void onPagePop(Route route) {
-    super.onPagePop(route);
-    print("AppLifecycleObserver - onPagePop");
-  }
-
-  @override
-  void onPageHide(Route route) {
-    super.onPageHide(route);
-    print("AppLifecycleObserver - onPageHide");
-  }
-
-  @override
-  void onPageShow(Route route) {
-    super.onPageShow(route);
-    print("AppLifecycleObserver - onPageShow");
-  }
-}
 
 void main() {
-  ///添加全局生命周期监听类
-  PageVisibilityBinding.instance.addGlobalObserver(AppLifecycleObserver());
-
-  ///这里的CustomFlutterBinding调用务必不可缺少，用于控制Boost状态的resume和pause
-  CustomFlutterBinding();
-
   runApp(MyApp());
 }
 
@@ -60,75 +11,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  /// 由于很多同学说没有跳转动画，这里是因为之前exmaple里面用的是 [PageRouteBuilder]，
-  /// 其实这里是可以自定义的，和Boost没太多关系，比如我想用类似iOS平台的动画，
-  /// 那么只需要像下面这样写成 [CupertinoPageRoute] 即可
-  /// (这里全写成[MaterialPageRoute]也行，这里只不过用[CupertinoPageRoute]举例子)
-  ///
-  /// 注意，如果需要push的时候，两个页面都需要动的话，
-  /// （就是像iOS native那样，在push的时候，前面一个页面也会向左推一段距离）
-  /// 那么前后两个页面都必须是遵循CupertinoRouteTransitionMixin的路由
-  /// 简单来说，就两个页面都是CupertinoPageRoute就好
-  /// 如果用MaterialPageRoute的话同理
-
-  static Map<String, FlutterBoostRouteFactory> routerMap = {
-    'mainPage': (settings, uniqueId) {
-      return CupertinoPageRoute(
-        settings: settings,
-        builder: (_) {
-          Map<String, Object> map = settings.arguments;
-          String data = map['data'];
-          return GestureDetector(
-            onTap: () {
-              Map<String, dynamic> arguments = Map();
-              arguments["naviTitle"] = "德玛西亚";
-              BoostNavigator.instance.push("goNative", arguments: arguments);
-            },
-            child: Container(
-              color: Colors.white,
-              child: Center(
-                child: Text(
-                  "mainPage $data",
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-      );
-    },
-    'simplePage': (settings, uniqueId) {
-      Map<String, Object> map = settings.arguments ?? {};
-      String data = map['data'] ?? "";
-      return CupertinoPageRoute(
-        settings: settings,
-        builder: (_) => Container(
-          color: Colors.white,
-          child: Center(
-            child: Text(
-              "simplePage $data",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          ),
-        ),
-      );
-    },
-  };
-
-  Route<dynamic> routeFactory(RouteSettings settings, String uniqueId) {
-    FlutterBoostRouteFactory func = routerMap[settings.name];
-    if (func == null) {
-      return null;
-    }
-    return func(settings, uniqueId);
-  }
-
   Widget appBuilder(Widget home) {
     return MaterialApp(
       home: home,
@@ -143,9 +25,17 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterBoostApp(
-      routeFactory,
-      appBuilder: appBuilder,
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          "德玛西亚",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
